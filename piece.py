513d3de2b4dd989ee0a211ef6545d82f88fc67bb
Charlie <66709630+charlie06728@users.noreply.github.com>
2021-03-30 11:17:38 +0800

    Add files via upload

--- piece.py
+++ piece.py
@@ -1,10 +1,12 @@
 """..."""
 from __future__ import annotations
 from typing import Optional
+import math
 
 
-DIRECTIONS = {'above', 'below', 'right', 'left', 'top right', 'top left',
-              'bottom right', 'bottom left'}
+# DIRECTIONS = {'above', 'below', 'right', 'left', 'top right', 'top left',
+#               'bottom right', 'bottom left'}
+DIRECTIONS = {'vertical', 'horizontal', 'right diagonal', 'left diagonal'}
 
 
 class Piece:
@@ -21,8 +23,8 @@ class Piece:
          'bottom right', 'bottom left'})
       """
     kind: str
-    neighbours: dict[str, Piece]
-    obstacles: dict[str, Piece]
+    neighbours: dict[str, list[Piece]]
+    obstacles: dict[str, list[Piece]]
     coordinate: tuple[int, int]
 
     def __init__(self, coordinate: tuple[int, int], kind: str) -> None:
@@ -30,6 +32,16 @@ class Piece:
         self.kind = kind
         self.neighbours = {}
         self.obstacles = {}
+
+    def is_adjacent(self, other: Piece) -> bool:
+        """Return whether a piece is adjacent to current one.
+        """
+        cur_cor = self.coordinate
+        other_cor = other.coordinate
+        if abs(cur_cor[0] - other_cor[0]) <= 1 and abs(cur_cor[1] - other_cor[1]) <= 1:
+            return True
+        else:
+            return False
 
 
 class Pieces:
@@ -58,19 +70,109 @@ class Pieces:
         all_neighbours = self.get_neighbours(piece.coordinate)
 
         for direction in all_neighbours:
-            if all_neighbours[direction].kind == piece.kind:
-                piece.neighbours[direction] = all_neighbours[direction]
-            else:
-                piece.obstacles[direction] = all_neighbours[direction]
+            for item in all_neighbours[direction]:
+                if item.kind == piece.kind:
+                    if direction in piece.neighbours:
+                        piece.neighbours[direction].append(item)
+                    else:
+                        piece.neighbours[direction] = [item]
+                else:
+                    if direction in piece.obstacles:
+                        piece.obstacles[direction].append(item)
+                    else:
+                        piece.obstacles = [item]
 
-    def get_neighbours(self, coordinate: tuple[int, int]) -> dict[str, Piece]:
+    def get_neighbours(self, coordinate: tuple[int, int]) -> dict[str, list[Piece]]:
         """Return the neighbours of given pieces.
         """
+        # TODO:
 
-    def evaluate(self) -> int:
+    def evaluate(self, visited: set[Piece]) -> int:
         """Return the score of the current situation.
         """
+        # ACCUMULATOR:
+        score_so_far = 0
 
-    def _pieces_in_a_line(self, c1: tuple[int, int]) -> list[Piece]:
-        """Return the piece in a line with the same color.
+        for coordinate in self.vertices:
+            visited += self.vertices[coordinate]
+
+    def _single_evaluation(self, coordinate: tuple[int, int], visited: set[Piece]) \
+            -> tuple[int, set]:
+        """...
+        current_piece = self.vertices[coordinate]
+        visited.add(self.vertices[coordinate])
+        neighbours = self.get_neighbours(current_piece.coordinate)
+        current_length = 5
+        score = 0
+        for direction in DIRECTIONS:
+            if direction in neighbours:
+                pieces_in_direction = neighbours[direction]
+                for piece in pieces_in_direction:
+                    # If the neighbouring piece doesn't have the same color, no score change will
+                    # be made
+                    if piece.kind == current_piece.kind and piece.kind == 'black':
+                        score += 50
+                    elif piece.kind == current_piece.kind and piece.kind == 'white':
+                        score -= 50
+                    visited.add(piece)
+                    score += self._single_evaluation(piece.coordinate, visited)
+
+        return score
         """
+        current_piece = self.vertices[coordinate]
+        visited.add(current_piece)
+        neighbours = self.get_neighbours(coordinate)
+        score_so_far = 0
+        for direction in DIRECTIONS:
+            pieces_in_dir = neighbours[direction]
+            favor_pieces = {current_piece}
+            for piece in pieces_in_dir:
+                counter_pieces = set()
+                if piece.kind == current_piece:
+                    favor_pieces.add(piece)
+                elif piece.kind != current_piece:
+                    counter_pieces.add(piece)
+
+                if len(counter_pieces) == 2:
+                    # This line has no possibility of being five in a row.
+                    score_so_far = 0
+                else:
+                    score_so_far += self._get_score(favor_pieces, counter_pieces)
+
+        return (score_so_far, visited)
+
+    def _get_score(self, pieces: set[Piece], obstacles: set[Piece]) -> int:
+        """Return the score of this line.
+        """
+        length = len(pieces)
+        obs_length = len(obstacles)
+        assert obs_length < 2
+        score = 0
+        if length == 1:
+            score = 50 - 25 * obs_length
+        elif length == 2 and obs_length == 0:
+            score = 200 - 100 * obs_length
+        elif length == 3 and obs_length == 0:
+            score = 500 - 200 * obs_length
+        elif length == 4:
+            if self._in_a_line(pieces):
+                score = 8000
+            else:
+                score = 800 - 300 * obs_length
+        elif length == 5:
+            # win
+            score = math.inf
+
+        return score
+
+    def _in_a_line(self, pieces: set[Piece]) -> bool:
+        """Return whether these pieces is in a line without gap.
+        """
+        co = pieces
+        for piece in pieces:
+            co.remove(piece)
+            for neighbour in co.copy():
+                if piece.is_adjacent(neighbour):
+                    co.remove(neighbour)
+
+        return len(co) == 0
