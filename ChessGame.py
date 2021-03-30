e0089c6800600aa9400d5e02b8d244d6a2235c08
jess-zhai <jessica.zhai@mail.utoronto.ca>
2021-03-29 23:52:06 -0400

    Update ChessGame.py
    updated find_valid_moves (created helper for it)

--- ChessGame.py
+++ ChessGame.py
@@ -1,7 +1,8 @@
 """A ChessGame class"""
 from piece import Piece, Pieces
 
-
+BOARD_WIDTH = 15
+BOARD_LENGTH = 15
 class ChessGame:
     """A class representing a state of a game.
 
@@ -36,9 +37,32 @@ class ChessGame:
         self.pieces.add_piece(new_piece)
         self._board[coordinate[0]][coordinate[1]] = player
 
+    def check_surrounding(self, piece: tuple[int, int], valid_moves: list) -> list[tuple]:
+        """Helper for get_valid_moves, checks the 5 by 5 surrounding of a piece and return the
+        unfilled positions. """
+        moves_for_piece = []
+        for i in range(piece[0] - 2, piece[0] + 3):
+            for j in range(piece[1] - 2, piece[1] + 3):
+                if i in range(0, BOARD_WIDTH) and j in range(0, BOARD_LENGTH):
+                    if self._board[i][j] is None and (i, j) not in valid_moves:
+                        moves_for_piece.append((i, j))
+
+        return moves_for_piece
+
     def get_valid_moves(self) -> list[tuple[int, int]]:
-        """Return a list of valid moves.
+        """Return a list of valid moves. When there is no valid moves, return an empty list.
+
+        NOTE: notice that when the board is empty, this function also returns an empty list.
+        So when making a move, make sure to check if previous_move is '*', and set the first
+        piece directly at the centre of the board.
         """
+        valid_moves = []
+        for row in self._board:
+            for piece in row:
+                if piece is not None:  # if there is a piece
+                    valid_moves.extend(self.check_surrounding(piece, valid_moves))
+
+        return valid_moves
 
     def get_score(self) -> int:
         """Return the socre of current situation.
