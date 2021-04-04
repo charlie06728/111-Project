ad9cfc53fbbbb3dc52bec54c81beebae782c5cce
Charlie <66709630+charlie06728@users.noreply.github.com>
2021-04-04 10:47:38 +0800

    Add files via upload

--- gametree.py
+++ gametree.py
@@ -2,7 +2,8 @@
 
 from __future__ import annotations
 from typing import List, Optional, Union
-import ChessGame
+from copy import deepcopy
+from ChessGame import ChessGame
 
 START_MOVE = '*'
 
@@ -18,14 +19,15 @@ class GameTree:
     Update the score if a step earns more score than before
 
     """
+    root: tuple[int, int]
     score: int
     move: Union[tuple[int, int], str]
     black_move: bool
-    subtrees: List[GameTree]
+    subtrees: Optional[list[GameTree]]
     better_score: int
-    game_status: ChessGame.ChessGame
+    game_status: ChessGame
 
-    def __init__(self, score: int = 0, move: str = '*', black_move: bool = True,
+    def __init__(self, score: Optional[int] = None, move: str = '*', black_move: bool = True,
                  better_score: int = 0):
         """Initialize the gametree to be an empty tree. """
         self.score = score
@@ -33,10 +35,10 @@ class GameTree:
         self.black_move = black_move  # In start of the game, black player moves first
         self.subtrees = []
         self.better_score = better_score
-        self.game_status = ChessGame.ChessGame()
+        self.game_status = ChessGame()
 
-    def generate_tree_based_on_move(self, game_state: ChessGame, prev_move: str,
-                                    depth: int) -> GameTree:
+    def generate_tree_based_on_move(self, game_state: ChessGame, prev_move: tuple[int, int],
+                                    depth: int) -> Optional[GameTree]:
         """ A recursive function that returns a tree.
         The root of the tree will be prev_move. The tree will first get all possible next moves,
         and for each "next move", it will recursively look for next moves of the move, and stop
@@ -46,9 +48,42 @@ class GameTree:
         will seek largest score, and when the move is for opponent, it will seek the lowest score.
         """
         if depth == 0:
+            self.root = prev_move
+            self.score = game_state.get_score()
             return self
         else:
-            ...
+            self.root = prev_move
+            valid_moves = game_state.get_valid_moves()
+
+            for move in valid_moves:
+                copy_game = deepcopy(game_state)
+                copy_game.make_move(move)
+                next_tree = GameTree(black_move=not self.black_move)
+                next_tree.better_score = self.better_score
+
+                self.add_subtree(next_tree.generate_tree_based_on_move(copy_game, move, depth - 1))
+
+                self.score = self.minimax()
+
+                # Alpha-Beta pruning
+                if self.black_move and self.score > next_tree.score:
+                    return None
+                elif not self.black_move and self.score < next_tree.score:
+                    return None
+
+    def minimax(self) -> int:
+        """Return the score that favor the current player most.
+        """
+        assert self.subtrees != []
+        score = []
+        for subtree in self.subtrees:
+            if subtree.score is not None:
+                score.append(subtree.score)
+
+        if self.black_move:
+            return max(score)
+        else:
+            return min(score)
 
     def add_subtree(self, subtree: GameTree) -> None:
         """Add the given subtree to self.subtrees. """
@@ -57,7 +92,7 @@ class GameTree:
 
     def update_score(self) -> None:
         """Update the score for the current tree based on the given chess game. """
-        self.score = ChessGame.ChessGame.get_score(self.game_status)
+        self.score = ChessGame.get_score(self.game_status)
 
     def get_max_score(self) -> tuple[tuple[int, int], int]:
         """Returns the a tuple of move and score of the subtree with highest score
