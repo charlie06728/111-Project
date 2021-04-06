2d11044f24867a8786e755e5981796db524d5641
Charlie <66709630+charlie06728@users.noreply.github.com>
2021-04-06 11:27:04 +0800

    Add files via upload

--- ChessGame.py
+++ ChessGame.py
@@ -21,7 +21,7 @@ class ChessGame:
     _valid_moves: list[str]
     _is_black_active: bool
     pieces: Pieces
-    prev_moves: Optional[tuple[int, int]]
+    prev_move: Optional[tuple[int, int]]
 
     def __init__(self, is_black_active: bool = True) -> None:
         """Initialize the Game"""
@@ -33,7 +33,7 @@ class ChessGame:
 
         self._is_black_active = is_black_active
         self.pieces = Pieces()
-        self.prev_moves = None
+        self.prev_move = None
 
     def make_move(self, coordinate: tuple[int, int]) -> None:
         """Make move on the chess game.
@@ -47,7 +47,7 @@ class ChessGame:
         self.pieces.add_piece(new_piece)
         self._board[coordinate[0]][coordinate[1]] = player
 
-        self.prev_moves = coordinate
+        self.prev_move = coordinate
 
     def check_surrounding(self, piece: tuple[int, int], valid_moves: list) -> list[tuple[int, int]]:
         """Helper for get_valid_moves, checks the 5 by 5 surrounding of a piece and return the
